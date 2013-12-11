cc7161da66e8a7de7b167b29fee3b57090e2f804
alexanderbeletsky <alexander.beletsky@gmail.com>
2013-12-11 19:06:28 +0200

    reverted

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 var seismo = require('seismo');
 var config = require('./config');
-var spawn = require('child_process').spawn;
 
-function startServer() {
-	seismo.start(config, function (err, app) {
-		var env = process.env.NODE_ENV || 'development';
-		console.log('seismo-server listening on port ' + app.get('port') + ' ' + env + ' mongo: ' + config.connection);
-	});
-}
+seismo.start(config, function (err, app) {
+	var env = process.env.NODE_ENV || 'development';
+	console.log('seismo-server listening on port ' + app.get('port') + ' ' + env + ' mongo: ' + config.connection);
+});
 
-spawn('/usr/bin/mongod');
-setTimeout(1000, startServer);
