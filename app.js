9139dc8f3fa2375cb8246af10ee1bb9dd2667176
alexanderbeletsky <alexander.beletsky@gmail.com>
2013-12-11 12:24:14 +0200

    ready

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 var seismo = require('seismo');
 var config = require('./config');
-var logger = require('./source/utils/logger');
 
 seismo.start(config, function (err, app) {
 	var env = process.env.NODE_ENV || 'development';
-	logger.info('seismo-server listening on port ' + app.get('port') + ' ' + env + ' mongo: ' + config.connection);
+	console.log('seismo-server listening on port ' + app.get('port') + ' ' + env + ' mongo: ' + config.connection);
 });
--- config/index.js
+++ config/index.js
@@ -1,6 +1,11 @@
-var util = require('util');
+var config = {
+	connection: 'mongodb://localhost:27017/analyticsdb',
+	authKey: 'ffbac26ce2b99cfc3c60a7eb911a108d4823ba1a',
+	tokenTtl: 60,
 
-var env = process.env.NODE_ENV || 'development';
-var config = util.format('/%s.config.js', env);
+	users: {
+		'seismo': '$2a$12$.Xp6U9nnZKVq78peM.M8ye1o2wd4H6YE40N/JMWEd7mqXidNGbEg2'
+	}
+};
 
-module.exports = require(__dirname + config);
+module.exports = config;
